8a01129b4871e696876181eb8c6c3e4265bf7f65
gabrielakerija <119396390+gabrielakerija@users.noreply.github.com>
2023-03-28 08:47:11 +0300

    pievienoju funkcijas

---
+++ 21.marts/script.js
@@ -0,0 +1,9 @@
+const button = document.querySelector(.js-plus)
+
+const plus = (cipars1, cipars2, cipars3)=> {
+   return cipars1 + cipars2 + cipars3
+}
+
+button.addEventListener('click', ()return => {
+return plus (1,2,3)
+})
--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const plusins = document.querySelector
-('.js-plus')
-const inputins = document.querySelector
-(js-input):
+const plusins = document.querySelector('.js-plus');
+const inputins = document.querySelector('.js-input');
 
 console.log(plusins)
 console.log(inputins)
+
+const Add = (cipars1, cipars2) => {
+return cipars1 + cipars2
+}
+
+const subtract = (cipars1, cipars2) => {
+    return cipars1 - cipars2
+}
+
+const multiply = (cipars1, cipars2) => {
+    return cipars1 * cipars2
+}
+
+const divide = (cipars1, cipars2) => {
+    return cipars1 / cipars2
+}
+
+plusins.addEventListener('click', ()=> {
+    console.log('mani nospieda')
+})
